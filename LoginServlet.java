b276da701ac0440901585d0049a50883262ca12d
nehakailasjagtap <59533450+nehakailasjagtap@users.noreply.github.com>
2020-02-16 19:06:56 +0530

    Add files via upload

---
+++ LoginServlet.java
@@ -0,0 +1,33 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet 
+{
+    public void doGet(HttpServletRequest req,HttpServletResponse resp)throws ServletException,IOException  
+    {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        
+        String st1=req.getParameter("user_name");
+        String st2=req.getParameter("user_password");
+        
+        if(st1.equals("nehajagtap")||st2.equals("neha123"))
+        {
+            out.print("<h3> WELCOME </h3>");
+        }
+        else
+        {
+             RequestDispatcher rr = req.getRequestDispatcher("index.html");
+             rr.forward(req,resp);
+        }
+
+    }
+    
+}
